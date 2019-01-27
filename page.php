196f857bb4ebe69eefc79f794a65a225738490de
Colby Johnson <colbyjohnson@Colbys-MacBook-Pro-2.local>
2019-01-26 22:50:10 -0500

    new commit

--- page.php
+++ page.php
@@ -8,22 +8,24 @@
 get_header(); ?>
 	
 	<div class="content">
-	
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-	
-		    <main class="main small-12 large-8 medium-8 cell" role="main">
-				
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+		<div class="grid-container">
+
+			<div class="inner-content grid-x grid-margin-x grid-padding-x">
+		
+			    <main class="main large-12 cell" role="main">
+					
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+				    
+				    <?php endwhile; endif; ?>							
+				    					
+				</main> <!-- end #main -->
 			    
-			    <?php endwhile; endif; ?>							
-			    					
-			</main> <!-- end #main -->
+			</div> <!-- end #inner-content -->
 
-		    <?php get_sidebar(); ?>
-		    
-		</div> <!-- end #inner-content -->
+		</div>
 
 	</div> <!-- end #content -->
 
